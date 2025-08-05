274ab7993735dfc6c587d6a76c25e920c7affdca
DANRAMAGE <ChiefDan@gmail.com>
2025-08-05 07:58:53 -0400

    Init checkin.

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from wqutilities import logging
-
-__version__  = "0.3.0"
-
-logger = logging.getLogger(__name__)
---
+++ wqutilities/__init__.py
@@ -0,0 +1,5 @@
+from wqutilities import logging
+
+__version__  = "0.3.0"
+
+logger = logging.getLogger(__name__)
